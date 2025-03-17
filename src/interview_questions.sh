fa4567f4cb6e4a41af395bdfd892f6e5b45ac46d
tmuhali <nbmuhali1209@gmail.com>
2025-03-17 21:18:42 +0200

    Create interview_questions.sh

---
+++ src/interview_questions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# src/interview_questions.sh
+
+QUESTIONS=(
+    "Can you explain the concept of polymorphism in object-oriented programming?"
+    "How do you handle error logging in your applications?"
+    "Describe your experience with continuous integration and continuous deployment."
+    # Add more questions as needed
+)
